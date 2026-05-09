9b5fa808db81feb58d3e121a24c1cbd87bd1203d
Srijito354 <srijitoghosh113@outlook.com>
2026-05-10 02:11:02 +0530

    Update

--- autonomous.py
+++ autonomous.py
@@ -20,7 +20,8 @@ app = socketio.WSGIApp(sio, Flask(__name__))
 speed_limit = 15
 
 model = Modela()
-model.load_state_dict(torch.load("trained_model_pre(non-panorama-only-center).pt"))
+#model.load_state_dict(torch.load("trained_model_pre(non-panorama-only-center).pt"))
+model.load_state_dict(torch.load("fine_tuned_resnet_400.pt"))
 model.eval()
 
 transform = transforms.Compose((
@@ -38,25 +39,20 @@ def send_control(steering, throttle):
         'steering_angle': str(steering),
         'throttle':       str(throttle),
         #'brake': str(brake)
-    }, namespace='/')
+    })
 
-@sio.on('connect', namespace='/')
+@sio.on('connect')
 def connect(sid, environ):
     print("Simulator connected!", sid)
     send_control(0, 0)
 
-@sio.on('telemetry', namespace='/')
+@sio.on('telemetry')
 def telemetry(sid, data):
     if data:
         speed = float(data['speed'])
-        #img = Image.open(BytesIO(base64.b64decode(data['image']))).convert("RGB")
-        img = np.array(Image.open(BytesIO(base64.b64decode(data['image']))))
-        print(img.shape)
-        #img = cv2.imread(BytesIO(base64.b64decode(data['image'])))
-        edges = cv2.Canny(img, threshold1=300, threshold2=400)
-        edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
+        img = Image.open(BytesIO(base64.b64decode(data['image']))).convert("RGB")
 
-        img_tensor = transform(edges).unsqueeze(0)
+        img_tensor = transform(img).unsqueeze(0)
 
         print(img_tensor.shape)
 
